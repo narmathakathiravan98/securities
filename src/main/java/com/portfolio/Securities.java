1f28aad5af7d18cdf30e6adccc9bf4039696cff7
narmathakathiravan98 <narmatha.kathiravan1998@gmail.com>
2025-11-27 12:52:56 -0500

    Registration of Investor added

--- src/main/java/com/portfolio/Securities.java
+++ src/main/java/com/portfolio/Securities.java
@@ -1,13 +1,6 @@
 package com.portfolio;
 
-import java.sql.CallableStatement;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
+import java.sql.*;
 import java.util.Properties;
 import java.util.Scanner;
 
@@ -48,18 +41,6 @@ public class Securities {
       System.out.println("Please restart the application.");
       System.exit(0);
     }
-
-    try{
-      if(!this.connection.isValid(5)) {
-        System.out.println("The application did not connect to the database");
-        System.out.println("Please restart the application.");
-        System.exit(0);
-      }
-    } catch (SQLException e) {
-      System.out.println("The application did not connect to the database");
-      System.out.println("Please restart the application.");
-      System.exit(0);
-    }
   }
 
   /**
@@ -76,14 +57,14 @@ public class Securities {
    * The flow of the application with all inputs and outputs.
    */
   public void run() {
-
+    Scanner scanner =  new Scanner(System.in);
     boolean isNotExit = true;
     while(isNotExit) {
       System.out.println("Menu options :");
       System.out.println("1 -> Create account");
       System.out.println("2 -> Log in");
       System.out.println("3 -> Exit the application");
-      System.out.println("Enter your choice : ");
+      System.out.print("Enter your choice : ");
       int option = scanner.nextInt();
       switch(option) {
         case 1:
@@ -101,46 +82,135 @@ public class Securities {
     System.out.println("Thank you for using the application!");
   }
 
-  private static void userCreateAccount() {
+  private void userCreateAccount() {
+      Scanner scanner = new Scanner(System.in);
 
+    System.out.println("Please provide the following details.");
+
+    System.out.print("First Name : ");
+    String firstName = scanner.next();
+    System.out.print("Last Name : ");
+    String lastName = scanner.next();
+    System.out.print("Username : ");
+    String username = scanner.next();
+    System.out.print("Password : ");
+    String password = scanner.next();
+    System.out.print("SSN :");
+    int ssn = scanner.nextInt();
+    System.out.print("Date of Birth (YYYY-MM-DD):");
+    String dateOfBirth = scanner.next();
+    System.out.print("Phone number : ");
+    String phoneNumber = scanner.next();
+    System.out.print("Address Street Number :");
+    int streetNumber = scanner.nextInt();
+    System.out.print("Address Street Name :");
+    String streetName = scanner.next();
+    System.out.print("Address City :");
+    String city = scanner.next();
+    System.out.print("Address State :");
+    String state = scanner.next();
+    System.out.print("Address ZipCode :");
+    int zipcode = scanner.nextInt();
+    System.out.print("Account category (Classic/Preferred/Standard) :");
+    String category = scanner.next();
+
+    try {
+
+        String sql = "INSERT INTO investor (first_name, last_name, username, password, category_type, ssn, date_of_birth, phone_number, " +
+                "address_street_number, address_street_name, address_city, address_state, address_zipcode) " +
+                "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+
+        PreparedStatement preparedStatement = this.connection.prepareStatement(sql);
+
+        preparedStatement.setString(1, firstName);
+        preparedStatement.setString(2, lastName);
+        preparedStatement.setString(3, username);
+        preparedStatement.setString(4, password);
+        preparedStatement.setString(5, category);
+        preparedStatement.setInt(6, ssn);
+        preparedStatement.setDate(7, Date.valueOf(dateOfBirth));
+        preparedStatement.setString(8, phoneNumber);
+        preparedStatement.setInt(9, streetNumber);
+        preparedStatement.setString(10, streetName);
+        preparedStatement.setString(11, city);
+        preparedStatement.setString(12, state);
+        preparedStatement.setInt(13, zipcode);
+
+        int rowsAffected = preparedStatement.executeUpdate();
+
+        if (rowsAffected > 0) {
+            System.out.println("Investor registered successful!");
+        } else {
+            System.out.println("Registration failed. Try again.");
+        }
+
+        preparedStatement.close();
+
+    } catch (Exception e) {
+            System.out.println(e.getMessage());
+            System.out.println("ERROR: Could not register user into the database. Try again.");
+    }
   }
 
-  private static void userLoginAndActions() {
+  private void userLoginAndActions() {
     System.out.println("Please enter username and password.");
     Scanner scanner =  new Scanner(System.in);
     String username = scanner.nextLine();
     String password = scanner.nextLine();
-    boolean isValidCredential = true;//getAuthentication(username, password);
-    if(!isValidCredential) {
+    int investorId = getInvestor(username, password);
+
+    if(investorId == 0) {
       System.out.println("Invalid credentials. Please create an account.");
     } else {
-      //Investor investor = new Investor();
       //System.out.println("Welcome " + investor.getFirstName() + " " + investor.getLastName());
+
       boolean isNotLoggedIn = true;
       while(isNotLoggedIn) {
         System.out.println("Dashboard options:");
         System.out.println("1 -> View Nominee");
         System.out.println("2 -> Add/Edit Nominee");
         System.out.println("3 -> Delete Nominee");
-        System.out.println("4 -> Show last 10 transactions");
-        System.out.println("5 -> Make a transaction");
-        System.out.println("6 -> Logout");
-        System.out.println("Enter your choice : ");
+        System.out.println("4 -> View portfolios");
+        System.out.println("5 -> Create/Modify portfolio");
+        System.out.println("6 -> Request for a portfolio manager");
+        System.out.println("7 -> Search for securities");
+        System.out.println("8 -> Show last 10 transactions");
+        System.out.println("9 -> Make a transaction");
+        System.out.println("10 -> Logout");
+        System.out.print("Enter your choice : ");
         int choice = scanner.nextInt();
         switch(choice) {
           case 1 :
+            viewNominee();
             break;
           case 2:
+            addOrEditNominee();
             break;
           case 3:
+            deleteNominee();
             break;
           case 4:
+            viewPortfolios();
             break;
           case 5:
+            createPortfolio();
             break;
           case 6:
+            requestPortfolioManager();
+            break;
+          case 7:
+            searchSecurities();
+            break;
+          case 8:
+            showLatestTransactions();
+            break;
+          case 9:
+            makeTransaction();
+            break;
+          case 10:
             isNotLoggedIn = false;
             break;
+          default: System.out.println("Invalid option.");
         }
       }
 
@@ -148,60 +218,50 @@ public class Securities {
     }
   }
 
-
-  /**
-   * Fetches all the spell types.
-   *
-   * @param connection the database connection.
-   * @return the list of spell types.
-   */
-  public List<String> getSpellTypeList(Connection connection) {
-    List<String> spellTypeList = new ArrayList<>();
-    String sql = "SELECT type_name FROM spell_type";
-    PreparedStatement preparedStatement;
-    try {
-      preparedStatement = connection.prepareStatement(sql);
-      ResultSet resultSet = preparedStatement.executeQuery();
-
-      while(resultSet.next()) {
-        spellTypeList.add(resultSet.getString("type_name"));
+  private int getInvestor(String username, String password) {
+      String sql = "";
+      try {
+          CallableStatement callableStatement = this.connection.prepareCall(sql);
+      } catch(Exception e) {
+          System.out.println(e.getMessage());
+          System.out.println("ERROR: Could not fetch the investor details. Try again.");
       }
-
-      resultSet.close();
-      preparedStatement.close();
-    } catch (SQLException e) {
-      System.out.println(e.getMessage());
-      System.out.println("ERROR: Could not fetch the spell types from database.");
-    }
-    return spellTypeList;
+      return 0;
   }
 
-  /**
-   * Prints all spells with the particular spell type.
-   * @param spellType the spell type to be queried for all spells.
-   * @param connection the database connection.
-   */
-  public void displaySpells(String spellType, Connection connection) {
-    String sql = "{ CALL spell_has_type(?) }";
-    CallableStatement callableStatement;
-    try {
+  private void viewNominee() {
 
-      callableStatement = connection.prepareCall(sql);
-      callableStatement.setString(1, spellType);
-      ResultSet  resultSet = callableStatement.executeQuery();
+  }
 
-      while (resultSet.next()) {
-        int id = resultSet.getInt("spell_id");
-        String name = resultSet.getString("spell_name");
-        String alias = resultSet.getString("spell_alias");
-        System.out.printf("ID: %d | Name: %s | Alias: %s%n", id, name, alias);
-      }
+  private void addOrEditNominee() {
 
-      resultSet.close();
-      callableStatement.close();
-    } catch (SQLException e) {
-      System.out.println(e.getMessage());
-      System.out.println("ERROR: Could not fetch the spells from database.");
-    }
+  }
+
+  private void deleteNominee() {
+
+  }
+
+  private void viewPortfolios() {
+
+  }
+
+  private void createPortfolio() {
+
+  }
+
+  private void requestPortfolioManager() {
+
+  }
+
+  private void searchSecurities() {
+
+  }
+
+  private void showLatestTransactions() {
+
+  }
+
+  private void makeTransaction() {
+
   }
 }
